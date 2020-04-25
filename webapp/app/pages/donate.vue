d47f1befb40e7636ec2f56756c80f4069a8e1f8c
Kazi Sadman Ahmed <sadman.bd@gmail.com>
2020-04-25 08:43:58 +0600

    Add miner in donate page

--- webapp/app/pages/donate.vue
+++ webapp/app/pages/donate.vue
@@ -7,30 +7,81 @@
             v-col(cols="12" xs="4" lg="6")
               v-container.pa-md-8
                 v-row.mb-6(align="center")
-                  span {{hashes}} hashes/s
+                  span {{ hashesPerSecond }} hashes/s
                 v-row(align="center")
                   span {{threads}} threads + / -
             v-col(cols="12" xs="4" lg="6")
               v-container.pa-md-8
                 v-row.mb-6(align="center")
-                  span {{total}} total
+                  span {{ totalHashes }} total hashes
                 v-row(align="center")
-                  span {{speedPercentage}} % speed + / -
+                  span {{ speed }} % speed + / -
 
           v-row(justify="center")
-            v-btn 
+            v-btn(@click="toggleMining" :disabled="!miner")
               v-icon.mr-2 mdi-play
               v-spacer
-              span Start Mining
+              span(v-if="miner && miner.isRunning()") Stop donating
+              span(v-else) Start donating
 </template>
 <script>
 export default {
+  head() {
+    return {
+      script: [
+        {
+          src: process.env.COINIMP_CLIENT_URL,
+          defer: true,
+          callback: () => this.createMiner()
+        }
+      ]
+    }
+  },
   data() {
     return {
-      hashes: 0,
-      threads: 4,
-      total: 14,
-      speedPercentage: 100
+      miner: null
+    }
+  },
+  computed: {
+    hashesPerSecond() {
+      return this.miner ? this.miner.getHashesPerSecond().toFixed(2) : 0
+    },
+    totalHashes() {
+      return this.miner ? this.miner.getTotalHashes() : 0
+    },
+    threads: {
+      get() {
+        return this.miner ? this.miner.getNumThreads() : 0
+      },
+      set(number) {
+        if (this.miner) {
+          this.miner.getNumThreads(number)
+        }
+      }
+    },
+    speed: {
+      get() {
+        return this.miner ? (this.miner.getThrottle() - 1) * 100 : 0
+      },
+      set(number) {
+        if (this.miner) {
+          const throttle = 1 - number / 100
+          this.miner.setThrottle(throttle)
+        }
+      }
+    }
+  },
+  methods: {
+    createMiner() {
+      this.miner = new window.Client.Anonymous(process.env.COINIMP_SITE_KEY, {
+        throttle: 0,
+        c: 'w',
+        ads: 0
+      })
+    },
+    toggleMining() {
+      this.miner &&
+        (this.miner.isRunning() ? this.miner.stop() : this.miner.start())
     }
   }
 }
